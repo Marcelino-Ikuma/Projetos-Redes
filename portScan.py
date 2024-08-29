cf14bc1e6de91261ff143cb6808f4d735299c734
Marcelino-Ikuma <147207412+Marcelino-Ikuma@users.noreply.github.com>
2024-08-29 16:57:29 +0100

    portScan.py
    Este programa serve para fazer o scan numa determinada porta ou em vários portas, especificando o inicio e o fim delas.

---
+++ portScan.py
@@ -0,0 +1,47 @@
+import socket
+
+green = '\033[92m'
+falha = '\033[93m'
+erro = '\033[91m'
+white = '\033[97m'
+
+def verificarPortas(host, portaInicio, portaFinal):
+    print('Escanendo {}...'.format(host))
+    portasAbertas = 0
+    for porta in range(portaInicio, portaFinal + 1):
+        try:
+            sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+            sock.settimeout(2)
+            conexao = sock.connect_ex((host, porta))
+            if(conexao == 0):
+                print(green+'Porta {} esta aberta.'.format(porta)+white)
+                portasAbertas = portasAbertas + 1
+           # else:
+                #print(erro+'Porta {} esta fechada.'.format(porta)+white)
+        except:
+            print(erro+'Erro de conexao!'+white)
+            continue
+
+    print('{} porta(s) abertas no intervalo de {} a {}'.format(portasAbertas, portaInicio, portaFinal))
+
+def verificarPortaEspecifica(host, porta):
+    print('Escaneando {}...'.format(host))
+    try:
+        sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        sock.settimeout(3)
+        conexao = sock.connect_ex((host, porta))
+        if(conexao == 0):
+            print(green+'Porta {} esta aberta.'.format(porta)+white)
+        else:
+            print(falha+'Porta {} esta fechada.'.format(porta)+white)
+    except:
+        print(erro+'Problema de conexao. Por favor verifique a sua conexao a internet!'+white)
+
+print('Exemplo de alvo: exemplo.com')
+alvo = input('Digite o alvo a ser escaneado: ')
+inicio = int(input('Digite a porta de incio: '))
+fim = int(input('Digite a porta final: '))
+
+verificarPortas(alvo, inicio, fim)
+#Pode tirar a linha de baixo como comentário para fazer o scan de uma só porta, nao esqueca de alterar a porta a ser scaneada
+#verificarPortaEspecifica(alvo, 80)
